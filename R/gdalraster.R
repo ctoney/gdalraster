ba2827d7ec9c2284d760f6c2dee9c580462c87cc
Chris Toney <jctoney@gmail.com>
2026-09-14 16:46:41 -0600

    doc: fix formatting of constructors in the Usage section

--- R/gdalraster.R
+++ R/gdalraster.R
@@ -49,9 +49,10 @@
 #' # to specify dataset open options:
 #' ds <- new(GDALRaster, filename, read_only = TRUE|FALSE, open_options)
 #' # to open without using shared mode:
-#' new(GDALRaster, filename, read_only, open_options, shared = FALSE)
+#' ds <- new(GDALRaster, filename, read_only, open_options, shared = FALSE)
 #' # to specify certain allowed driver(s):
-#' new(GDALRaster, filename, read_only, open_options, shared, allowed_drivers)
+#' ds <- new(GDALRaster, filename, read_only, open_options, shared,
+#'           allowed_drivers)
 #'
 #' ## Read/write fields (per-object settings)
 #' ds$infoOptions
